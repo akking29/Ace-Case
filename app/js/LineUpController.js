f414fb2f5a96a9d8b05fa51c863de596b3ad9dfa
Adam King <Adam King>
2016-07-07 14:28:39 -0700

    recent

--- app/js/LineUpController.js
+++ app/js/LineUpController.js
@@ -5,39 +5,79 @@
         .module('app')
         .controller('LineUpController', LineUpController);
 
-    LineUpController.$inject = ['$scope', '$log', 'LineUpFactory', "NgTableParams"];
+    LineUpController.$inject = ['$scope', '$log', 'LineUpFactory'];
 
-    function LineUpController($scope, $log, LineUpFactory, NgTableParams) {
+    function LineUpController($scope, $log, LineUpFactory) {
 
         var vm = this;
         vm.ctrlName = 'LineUpController';
+        vm.lineup = [
+            { Position: "P", player: null },
+            { Position: "P", player: null },
+            { Position: "C", player: null },
+            { Position: "1B", player: null },
+            { Position: "2B", player: null },
+            { Position: "3B", player: null },
+            { Position: "SS", player: null },
+            { Position: "OF", player: null },
+            { Position: "OF", player: null },
+            { Position: "OF", player: null }
+        ];
 
-        vm.getGames = function() {
-            LineUpFactory.getGames().then(
+        vm.init = function() {
+            var data = {};
+
+            vm.getGames(data).then(vm.getPlayers).then(function(data) {
+                vm.players = data.players;
+                vm.games = data.games;
+
+                console.log(vm.games);
+                console.log(vm.players);
+
+            });
+        }
+
+        vm.getGames = function(data) {
+
+            return LineUpFactory.getGames().then(
                 function(response) {
-                    vm.games = response.data;
-                    console.log(vm.games);
+                    data.games = response.data;
+                    return data;
                 });
         };
 
-        vm.getPlayers = function() {
-            LineUpFactory.getPlayers().then(
+        vm.getPlayers = function(data) {
+            return LineUpFactory.getPlayers().then(
                 function(response) {
-                    vm.players = response.data;
-
-                    vm.tableParams = new NgTableParams({}, {
-                        dataset: vm.players
-                    });
-
-                    console.log(vm.players);
+                    data.players = response.data;
+                    return data;
                 });
         };
 
-        vm.getGames();
-        vm.getPlayers();
+        vm.init();
 
-        //content
+        vm.addPlayer = function(player) {
+            var lineup = null;
+            console.log(lineup);
+
+            for (var i = 0; i < vm.lineup.length; i++) {
+                if (vm.lineup[i].Position === player.FanDuelPosition && vm.lineup[i].player === null) {
+                    vm.lineup[i].player = player;
+                    vm.players.splice(vm.players.indexOf(player), 1);
+                    console.log(vm.lineup);
+                    console.log(player);
+                    return;
+                } if (vm.lineup[i].Position === player.FanDuelPosition && vm.lineup[i].player === vm.lineup[i].player || vm.lineup[1].player !== null){
+                    alert("position already filled");
+                }
+            }
+        };
+
+        vm.nixPlayer = function(lineup) {
+            vm.players.unshift(lineup.player);
+            lineup.player = null;
+        
+        };
     }
-
 
 })();
--- app/js/LineUpFactory.js
+++ app/js/LineUpFactory.js
@@ -1,82 +1,64 @@
 (function() {
-        'use strict';
+    'use strict';
 
-        angular
-            .module('app')
-            .factory('LineUpFactory', LineUpFactory);
+    angular
+        .module('app')
+        .factory('LineUpFactory', LineUpFactory);
 
-        LineUpFactory.$inject = ['$http', '$q', '$log'];
+    LineUpFactory.$inject = ['$http', '$q', '$log'];
 
-        function LineUpFactory($http, $q, $log) {
+    function LineUpFactory($http, $q, $log) {
 
-            var service = {
-
-                getGames: getGames,
-                getPlayers: getPlayers
-            };
-            //content
-
-
-            return service;
-
-            function getGames() {
-                var defer = $q.defer();
-
-                $http({
-                    method: 'GET',
-                    url: 'https://api.fantasydata.net/mlb/v2/JSON/GamesByDate/2016-JUN-22?key=1d7a3c97b3e742b9b1662604691b3cab'
-                }).then(
-                    function(response) {
-                        if (typeof response.data === 'object') {
-                            defer.resolve(response);
-
-                        } else {
-                            defer.reject(response);
-                        }
-                    },
-                    function(error) {
-                        defer.reject(error);
-                        $log.error(error);
-                    });
-                return defer.promise;
-            };
-
-            function getPlayers() {
-                var defer = $q.defer();
-
-                $http({
-                    method: 'GET',
-                    url: 'https://api.fantasydata.net/mlb/v2/JSON/PlayerGameStatsByDate/2016-JUN-22?key=1d7a3c97b3e742b9b1662604691b3cab'
-                }).then(
-                    function(response) {
-                        if (typeof response.data === 'object') {
-                            defer.resolve(response);
-                        } else {
-                            defer.reject(response);
-                        }
-                    },
-                    function(error) {
-                        defer.reject(error);
-                        $log.error(error);
-                    });
-                return defer.promise;
-                };
+        var service = {
+            getGames: getGames,
+            getPlayers: getPlayers
         };
+        //content
 
-        /*$http.jsonp('https://api.fantasydata.net/mlb/v2/JSONP/GamesByDate/2016-JUN-22?key=1d7a3c97b3e742b9b1662604691b3cab&CALLBACK=JSON_CALLBACK').then(
+        return service;
+
+        function getGames() {
+            var defer = $q.defer();
+
+            $http({
+                method: 'GET',
+                url: 'https://api.fantasydata.net/mlb/v2/JSON/GamesByDate/2016-JUN-29?key=1d7a3c97b3e742b9b1662604691b3cab'
+            }).then(
                 function(response) {
-                    defer.resolve(response.data);
+                    if (typeof response.data === 'object') {
+                        defer.resolve(response);
+
+                    } else {
+                        defer.reject(response);
+                    }
                 },
                 function(error) {
                     defer.reject(error);
                     $log.error(error);
-                }
-            );
-
+                });
             return defer.promise;
-        }*/
+        };
 
+        function getPlayers() {
+            var defer = $q.defer();
 
-    
+            $http({
+                method: 'GET',
+                url: 'https://api.fantasydata.net/mlb/v2/JSON/PlayerGameStatsByDate/2016-JUN-22?key=1d7a3c97b3e742b9b1662604691b3cab'
+            }).then(
+                function(response) {
+                    if (typeof response.data === 'object') {
+                        defer.resolve(response);
+                    } else {
+                        defer.reject(response);
+                    }
+                },
+                function(error) {
+                    defer.reject(error);
+                    $log.error(error);
+                });
+            return defer.promise;
+        };
+    };
 
 })();
